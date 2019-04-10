4ce524feb1eda9a42187e1c3a12f9c2c58ebec82
Nick Elsey <nicholas.elsey@gmail.com>
2019-04-10 18:14:16 -0400

    new version of SL18h library, include SMD

--- SL18h/StPicoEvent/StPicoHelix.h
+++ SL18h/StPicoEvent/StPicoHelix.h
@@ -16,7 +16,6 @@
 #include <math.h>
 #include <utility>
 #include <algorithm>
-#include <cmath>
 
 // ROOT headers
 #include "TVector3.h"
@@ -219,10 +218,10 @@ inline Double_t StPicoHelix::pathLength(Double_t X, Double_t Y) const { return f
 inline Int_t StPicoHelix::bad(Double_t WorldSize) const {
 
   Int_t ierr;
-  if ( !std::isfinite(mDipAngle) ) {
+  if ( !::finite(mDipAngle) ) {
     return 11;
   }
-  if ( !std::isfinite(mCurvature) ) {
+  if ( !::finite(mCurvature) ) {
     return 12;
   }
   
@@ -241,7 +240,7 @@ inline Int_t StPicoHelix::bad(Double_t WorldSize) const {
     default: tmpVal = NAN;
     };
     
-    if ( !std::isfinite( tmpVal ) ) {
+    if ( !::finite( tmpVal ) ) {
       ierr = 10 + iIter;
     }
     if ( ::fabs( tmpVal ) > WorldSize ) {
